98444c0c60169004e85ceb06e0cf8427fbefa63a
Rayhan Kobir <52678141+rayhankobir-dev@users.noreply.github.com>
2024-02-21 17:00:02 +0600

    Update redis.js

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,5 +1,5 @@
 import { Redis } from "ioredis";
 import { redisConfig } from "../config.js";
 
-const redisClient = new Redis("rediss://default:AVNS_Ud8-Yz18eqn4Hqw22PW@obeey-rayhankobir793-2757.a.aivencloud.com:16555");
+const redisClient = new Redis(redisConfig.url);
 export default redisClient;
