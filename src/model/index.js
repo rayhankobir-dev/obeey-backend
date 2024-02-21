ba1cb9e817efab29c14c241f7ecd32c4d6761c90
Rayhan Kobir <52678141+rayhankobir-dev@users.noreply.github.com>
2024-02-21 12:03:52 +0600

    Update prisma

--- src/model/index.js
+++ src/model/index.js
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+const prismaClient = new PrismaClient("postgres://avnadmin:AVNS_O9pthBaMZmxzgmhUo89@obeey-pg-server-rayhankobir793-2757.a.aivencloud.com:16554/defaultdb?sslmode=require");
 export default prismaClient;
